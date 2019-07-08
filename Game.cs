72bd583aa20f742fab4dc682fd0dcdcb0dc5324f
Chris Firgens <firgenscj@gmail.com>
2019-07-08 10:36:18 -0500

    Added in Weather, Game, Inventory, Weather Classes; implemented weather methods to get random weather and random temperature, and a method to combine the two into a string;

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonade_Stand
+{
+    class Game
+    {
+
+        public void RunGame()
+        {
+            Weather weather = new Weather();
+
+            weather.CombinedWeather();
+
+            Console.ReadLine();
+            weather.CombinedWeather();
+
+            Console.ReadLine();
+            weather.CombinedWeather();
+
+            Console.ReadLine();
+            weather.CombinedWeather();
+            weather.CombinedWeather();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonade_Stand
+{
+    class Weather
+    {
+
+        //Has
+
+        //Constructor
+
+        //Can do
+
+        public string GetWeather()
+        {
+            List<string> weather = new List<string>() { "Clear and Sunny", "Rainy", "Overcast", "Hazy"};
+            int weatherNumber = new Random().Next(0, 3);
+            string weatherToday = (weather[weatherNumber]);
+            return weatherToday;
+        }
+        
+        public int GetTemperature()
+        {
+            int temperature = new Random().Next(65, 110);
+            return temperature;
+        }
+
+        public void CombinedWeather()
+        {
+            string CombinedWeather = (GetTemperature() + " and " + GetWeather());
+            Console.WriteLine("Today's weather is " + CombinedWeather);
+
+        }
+    }
+}
